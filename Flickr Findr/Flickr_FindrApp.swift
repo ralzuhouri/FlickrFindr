19973ef8ef3266b12a114ab81ad781c56ff623e2
Ramy Al Zuhouri <ramy@klikkie.com>
2023-02-15 17:01:53 +0100

    Project setup

---
+++ Flickr Findr/Flickr_FindrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flickr_FindrApp.swift
+//  Flickr Findr
+//
+//  Created by Ramy Al Zuhouri on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Flickr_FindrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
